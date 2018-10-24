06b590b83d7a5ede44a5d91932b812b977dd122e
huimintoh <hmintoh@gmail.com>
2018-10-24 10:18:44 +0800

    set up in memory mongodb server for tests

---
+++ test_helper/in_memory_mongodb_setup.js
@@ -0,0 +1,31 @@
+//set up in memory mongodb server, beforeEach for tests
+const mongoose = require("mongoose");
+const mongodbMemoryServer = require("mongodb-memory-server").default;
+
+//explicit declaration for mongoose promises to follow behavior of default promise
+mongoose.Promise = global.Promise;
+
+//timeout to break from db invalid queries = 60 sec
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+
+const mongoServer = new mongodbMemoryServer();
+
+const setup = () => {
+  const mongoURI = await mongoServer.getConnectionString();
+  await mongoose
+    .connect(mongoURI, {useNewUrlParser: true})
+    .then(
+      () => console.log("database is ready"), 
+      error => console.error(error)
+    )
+};
+
+const teardown = () => {
+  mongoose.disconnect();
+  mongoServer.stop();
+};
+
+module.exports = {
+  setup,
+  teardown
+};
